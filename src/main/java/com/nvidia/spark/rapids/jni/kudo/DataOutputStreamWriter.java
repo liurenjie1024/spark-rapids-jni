5223f7c2b8fed31750fe09780a5f24ab243de678
liurenjie1024 <liurenjie2008@gmail.com>
2024-11-28 13:02:01 +0800

    Increse buffer

--- src/main/java/com/nvidia/spark/rapids/jni/kudo/DataOutputStreamWriter.java
+++ src/main/java/com/nvidia/spark/rapids/jni/kudo/DataOutputStreamWriter.java
@@ -25,7 +25,7 @@ import java.io.IOException;
  * Visible for testing
  */
 class DataOutputStreamWriter extends DataWriter {
-  private final byte[] arrayBuffer = new byte[1024];
+  private final byte[] arrayBuffer = new byte[8*1024];
   private final DataOutputStream dout;
 
   public DataOutputStreamWriter(DataOutputStream dout) {
