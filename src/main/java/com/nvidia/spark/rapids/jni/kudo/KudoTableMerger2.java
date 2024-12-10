fa0c58893be6ddba8ba9f14854ec495a83eaedb2
liurenjie1024 <liurenjie2008@gmail.com>
2024-12-10 17:48:43 +0800

    Debug

--- src/main/java/com/nvidia/spark/rapids/jni/kudo/KudoTableMerger2.java
+++ src/main/java/com/nvidia/spark/rapids/jni/kudo/KudoTableMerger2.java
@@ -325,7 +325,9 @@ class KudoTableMerger2 extends MultiKudoTableVisitor2<Void, Void, KudoHostMergeR
     return Arms.closeIfException(HostMemoryBuffer.allocate(mergedInfo.getTotalDataLen()),
         buffer -> {
           KudoTableMerger2 merger = new KudoTableMerger2(serializedTables, buffer, mergedInfo.getColumnOffsets());
-          return Visitors.visitSchema(schema, merger);
+          KudoHostMergeResult result = Visitors.visitSchema(schema, merger);
+          System.out.println("KudoTableMerger2: " + result);
+          return result;
         });
   }
 }
