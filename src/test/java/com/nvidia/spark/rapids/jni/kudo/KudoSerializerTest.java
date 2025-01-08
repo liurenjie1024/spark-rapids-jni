399fcdfe09e155719df5168d763c6a71b9457ebf
Renjie Liu <liurenjie2008@gmail.com>
2025-01-08 16:21:46 +0800

    Remove complicated type

--- src/test/java/com/nvidia/spark/rapids/jni/kudo/KudoSerializerTest.java
+++ src/test/java/com/nvidia/spark/rapids/jni/kudo/KudoSerializerTest.java
@@ -331,98 +331,98 @@ public class KudoSerializerTest {
             null, 5, null, 7, null, 9, null, 11, null, 13, null, 15)
         .decimal64Column(-8, 1L, null, 1001L, 50L, -2000L, null, 1L, 2L, 3L,
             4L, null, 6L, 7L, 8L, 9L, null, 11L, 12L, 13L, 14L, null)
-        .column("A", "B", "C", "D", null, "TESTING", "1", "2", "3", "4",
-            "5", "6", "7", null, "9", "10", "11", "12", "13", null, "15")
-        .column("A", "A", "C", "C", "E", "TESTING", "1", "2", "3", "4", "5",
-            "6", "7", "", "9", "10", "11", "12", "13", "", "15")
-        .column("", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-            "", "", "", "", "", "")
-        .column("", null, "", "", null, "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-            "")
-        .column((String) null, null, null, null, null, null, null, null, null, null,
-            null, null, null, null, null, null, null, null, null, null, null)
-        .column(mapStructType, structs(struct("1", "2")), structs(struct("3", "4")), null, null,
-            structs(struct("key", "value"), struct("a", "b")), null, null,
-            structs(struct("3", "4"), struct("1", "2")), structs(),
-            structs(null, struct("foo", "bar")),
-            structs(null, null, null), null, null, null, null, null, null, null, null, null,
-            structs(struct("the", "end")))
-        .column(structType, struct(1, 1f), null, struct(2, 3f),
-            null, struct(8, 7f), struct(0, 0f), null,
-            null, struct(-1, -1f), struct(-100, -100f),
-            struct(Integer.MAX_VALUE, Float.MAX_VALUE), null,
-            null, null,
-            null, null,
-            null, null,
-            null, null,
-            struct(Integer.MIN_VALUE, Float.MIN_VALUE))
-        .column(integers(1, 2), null, integers(3, 4, null, 5, null), null, null, integers(6, 7, 8),
-            integers(null, null, null), integers(1, 2, 3), integers(4, 5, 6), integers(7, 8, 9),
-            integers(10, 11, 12), integers((Integer) null), integers(14, null),
-            integers(14, 15, null, 16, 17, 18), integers(19, 20, 21), integers(22, 23, 24),
-            integers(25, 26, 27), integers(28, 29, 30), integers(31, 32, 33), null,
-            integers(37, 38, 39))
-        .column(integers(), integers(), integers(), integers(), integers(), integers(), integers(),
-            integers(), integers(), integers(), integers(), integers(), integers(), integers(),
-            integers(), integers(), integers(), integers(), integers(), integers(), integers())
-        .column(integers(null, null), integers(null, null, null, null), integers(),
-            integers(null, null, null), integers(), integers(null, null, null, null, null),
-            integers((Integer) null), integers(null, null, null), integers(null, null),
-            integers(null, null, null, null), integers(null, null, null, null, null), integers(),
-            integers(null, null, null, null), integers(null, null, null), integers(null, null),
-            integers(null, null, null), integers(null, null), integers((Integer) null),
-            integers((Integer) null), integers(null, null),
-            integers(null, null, null, null, null))
-        .column((Integer) null, null, null, null, null, null, null, null, null, null,
-            null, null, null, null, null, null, null, null, null, null, null)
-        .column(strings("1", "2", "3"), strings("4"), strings("5"), strings("6, 7"),
-            strings("", "9", null), strings("11"), strings(""), strings(null, null),
-            strings("15", null), null, null, strings("18", "19", "20"), null, strings("22"),
-            strings("23", ""), null, null, null, null, strings(), strings("the end"))
-        .column(strings(), strings(), strings(), strings(), strings(), strings(), strings(),
-            strings(), strings(), strings(), strings(), strings(), strings(), strings(), strings(),
-            strings(), strings(), strings(), strings(), strings(), strings())
-        .column(strings(null, null), strings(null, null, null, null), strings(),
-            strings(null, null, null), strings(), strings(null, null, null, null, null),
-            strings((String) null), strings(null, null, null), strings(null, null),
-            strings(null, null, null, null), strings(null, null, null, null, null), strings(),
-            strings(null, null, null, null), strings(null, null, null), strings(null, null),
-            strings(null, null, null), strings(null, null), strings((String) null),
-            strings((String) null), strings(null, null),
-            strings(null, null, null, null, null))
-        .column((String) null, null, null, null, null, null, null, null, null, null,
-            null, null, null, null, null, null, null, null, null, null, null)
-        .column(listMapType, asList(asList(struct("k1", "v1"), struct("k2", "v2")),
-                singletonList(struct("k3", "v3"))),
-            asList(asList(struct("k4", "v4"), struct("k5", "v5"),
-                struct("k6", "v6")), singletonList(struct("k7", "v7"))),
-            null, null, null, asList(asList(struct("k8", "v8"), struct("k9", "v9")),
-                asList(struct("k10", "v10"), struct("k11", "v11"), struct("k12", "v12"),
-                    struct("k13", "v13"))),
-            singletonList(asList(struct("k14", "v14"), struct("k15", "v15"))), null, null, null,
-            null,
-            asList(asList(struct("k16", "v16"), struct("k17", "v17")),
-                singletonList(struct("k18", "v18"))),
-            asList(asList(struct("k19", "v19"), struct("k20", "v20")),
-                singletonList(struct("k21", "v21"))),
-            asList(singletonList(struct("k22", "v22")), singletonList(struct("k23", "v23"))),
-            asList(null, null, null),
-            asList(singletonList(struct("k22", null)), singletonList(struct("k23", null))),
-            null, null, null, null, null)
-        .column(listDateType, asList(struct(-210, 293), struct(-719, 205), struct(-509, 183),
-                struct(174, 122), struct(647, 683)),
-            asList(struct(311, 992), struct(-169, 482), struct(166, 525)),
-            asList(struct(156, 197), struct(926, 134), struct(747, 312), struct(293, 801)),
-            asList(struct(647, null), struct(293, 387)), emptyList(),
-            null, emptyList(), null,
-            asList(struct(-210, 293), struct(-719, 205), struct(-509, 183), struct(174, 122),
-                struct(647, 683)),
-            asList(struct(311, 992), struct(-169, 482), struct(166, 525)),
-            asList(struct(156, 197), struct(926, 134), struct(747, 312), struct(293, 801)),
-            asList(struct(647, null), struct(293, 387)), emptyList(), null,
-            emptyList(), null,
-            singletonList(struct(778, 765)), asList(struct(7, 87), struct(8, 96)),
-            asList(struct(9, 56), struct(10, 532), struct(11, 456)), null, emptyList())
+//        .column("A", "B", "C", "D", null, "TESTING", "1", "2", "3", "4",
+//            "5", "6", "7", null, "9", "10", "11", "12", "13", null, "15")
+//        .column("A", "A", "C", "C", "E", "TESTING", "1", "2", "3", "4", "5",
+//            "6", "7", "", "9", "10", "11", "12", "13", "", "15")
+//        .column("", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+//            "", "", "", "", "", "")
+//        .column("", null, "", "", null, "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+//            "")
+//        .column((String) null, null, null, null, null, null, null, null, null, null,
+//            null, null, null, null, null, null, null, null, null, null, null)
+//        .column(mapStructType, structs(struct("1", "2")), structs(struct("3", "4")), null, null,
+//            structs(struct("key", "value"), struct("a", "b")), null, null,
+//            structs(struct("3", "4"), struct("1", "2")), structs(),
+//            structs(null, struct("foo", "bar")),
+//            structs(null, null, null), null, null, null, null, null, null, null, null, null,
+//            structs(struct("the", "end")))
+//        .column(structType, struct(1, 1f), null, struct(2, 3f),
+//            null, struct(8, 7f), struct(0, 0f), null,
+//            null, struct(-1, -1f), struct(-100, -100f),
+//            struct(Integer.MAX_VALUE, Float.MAX_VALUE), null,
+//            null, null,
+//            null, null,
+//            null, null,
+//            null, null,
+//            struct(Integer.MIN_VALUE, Float.MIN_VALUE))
+//        .column(integers(1, 2), null, integers(3, 4, null, 5, null), null, null, integers(6, 7, 8),
+//            integers(null, null, null), integers(1, 2, 3), integers(4, 5, 6), integers(7, 8, 9),
+//            integers(10, 11, 12), integers((Integer) null), integers(14, null),
+//            integers(14, 15, null, 16, 17, 18), integers(19, 20, 21), integers(22, 23, 24),
+//            integers(25, 26, 27), integers(28, 29, 30), integers(31, 32, 33), null,
+//            integers(37, 38, 39))
+//        .column(integers(), integers(), integers(), integers(), integers(), integers(), integers(),
+//            integers(), integers(), integers(), integers(), integers(), integers(), integers(),
+//            integers(), integers(), integers(), integers(), integers(), integers(), integers())
+//        .column(integers(null, null), integers(null, null, null, null), integers(),
+//            integers(null, null, null), integers(), integers(null, null, null, null, null),
+//            integers((Integer) null), integers(null, null, null), integers(null, null),
+//            integers(null, null, null, null), integers(null, null, null, null, null), integers(),
+//            integers(null, null, null, null), integers(null, null, null), integers(null, null),
+//            integers(null, null, null), integers(null, null), integers((Integer) null),
+//            integers((Integer) null), integers(null, null),
+//            integers(null, null, null, null, null))
+//        .column((Integer) null, null, null, null, null, null, null, null, null, null,
+//            null, null, null, null, null, null, null, null, null, null, null)
+//        .column(strings("1", "2", "3"), strings("4"), strings("5"), strings("6, 7"),
+//            strings("", "9", null), strings("11"), strings(""), strings(null, null),
+//            strings("15", null), null, null, strings("18", "19", "20"), null, strings("22"),
+//            strings("23", ""), null, null, null, null, strings(), strings("the end"))
+//        .column(strings(), strings(), strings(), strings(), strings(), strings(), strings(),
+//            strings(), strings(), strings(), strings(), strings(), strings(), strings(), strings(),
+//            strings(), strings(), strings(), strings(), strings(), strings())
+//        .column(strings(null, null), strings(null, null, null, null), strings(),
+//            strings(null, null, null), strings(), strings(null, null, null, null, null),
+//            strings((String) null), strings(null, null, null), strings(null, null),
+//            strings(null, null, null, null), strings(null, null, null, null, null), strings(),
+//            strings(null, null, null, null), strings(null, null, null), strings(null, null),
+//            strings(null, null, null), strings(null, null), strings((String) null),
+//            strings((String) null), strings(null, null),
+//            strings(null, null, null, null, null))
+//        .column((String) null, null, null, null, null, null, null, null, null, null,
+//            null, null, null, null, null, null, null, null, null, null, null)
+//        .column(listMapType, asList(asList(struct("k1", "v1"), struct("k2", "v2")),
+//                singletonList(struct("k3", "v3"))),
+//            asList(asList(struct("k4", "v4"), struct("k5", "v5"),
+//                struct("k6", "v6")), singletonList(struct("k7", "v7"))),
+//            null, null, null, asList(asList(struct("k8", "v8"), struct("k9", "v9")),
+//                asList(struct("k10", "v10"), struct("k11", "v11"), struct("k12", "v12"),
+//                    struct("k13", "v13"))),
+//            singletonList(asList(struct("k14", "v14"), struct("k15", "v15"))), null, null, null,
+//            null,
+//            asList(asList(struct("k16", "v16"), struct("k17", "v17")),
+//                singletonList(struct("k18", "v18"))),
+//            asList(asList(struct("k19", "v19"), struct("k20", "v20")),
+//                singletonList(struct("k21", "v21"))),
+//            asList(singletonList(struct("k22", "v22")), singletonList(struct("k23", "v23"))),
+//            asList(null, null, null),
+//            asList(singletonList(struct("k22", null)), singletonList(struct("k23", null))),
+//            null, null, null, null, null)
+//        .column(listDateType, asList(struct(-210, 293), struct(-719, 205), struct(-509, 183),
+//                struct(174, 122), struct(647, 683)),
+//            asList(struct(311, 992), struct(-169, 482), struct(166, 525)),
+//            asList(struct(156, 197), struct(926, 134), struct(747, 312), struct(293, 801)),
+//            asList(struct(647, null), struct(293, 387)), emptyList(),
+//            null, emptyList(), null,
+//            asList(struct(-210, 293), struct(-719, 205), struct(-509, 183), struct(174, 122),
+//                struct(647, 683)),
+//            asList(struct(311, 992), struct(-169, 482), struct(166, 525)),
+//            asList(struct(156, 197), struct(926, 134), struct(747, 312), struct(293, 801)),
+//            asList(struct(647, null), struct(293, 387)), emptyList(), null,
+//            emptyList(), null,
+//            singletonList(struct(778, 765)), asList(struct(7, 87), struct(8, 96)),
+//            asList(struct(9, 56), struct(10, 532), struct(11, 456)), null, emptyList())
         .build();
   }
 
@@ -450,10 +450,10 @@ public class KudoSerializerTest {
           try (Table merged = serializer.mergeToTable(kudoTables).getLeft()) {
             assertEquals(expected.getRowCount(), merged.getRowCount());
 
-//            TableDebug.builder()
-//                    .withOutput(TableDebug.Output.STDOUT)
-//                    .build()
-//                    .debug("Merged table", merged);
+            TableDebug.builder()
+                    .withOutput(TableDebug.Output.STDOUT)
+                    .build()
+                    .debug("Merged table", merged);
 
             AssertUtils.assertTablesAreEqual(expected, merged);
           }
