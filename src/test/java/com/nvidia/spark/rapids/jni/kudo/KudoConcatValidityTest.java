e8480f903611004394cf13d682c4a2cbb5dacee5
Ray Liu <liurenjie2008@gmail.com>
2025-01-23 17:20:45 +0800

    Fix Case 3

--- src/test/java/com/nvidia/spark/rapids/jni/kudo/KudoConcatValidityTest.java
+++ src/test/java/com/nvidia/spark/rapids/jni/kudo/KudoConcatValidityTest.java
@@ -161,6 +161,32 @@ public class KudoConcatValidityTest {
         }
     }
 
+    // When srcBitIdx > destBitIdx, srcIntBufLen = 1
+    @Test
+    public void testConcatValidityCase6() {
+        Random random = new Random(7788);
+        int accuArrLen = 0;
+        // Be careful with startRow, they are carefully designed to cover all test cases.
+        try (HostMemoryBuffer dest = HostMemoryBuffer.allocate(4096)) {
+            // When srcBitIdx == destBitIdx
+            ValidityConcatArray arr1 = new ValidityConcatArray(0, 14, random, "Array 1",
+                    accuArrLen);
+            arr1.appendToDest(dest);
+            accuArrLen += arr1.array.length;
+
+            // destBitIdx = 14
+            ValidityConcatArray arr2 = new ValidityConcatArray(17, 9, random, "Array 2",
+                    accuArrLen);
+            arr2.appendToDest(dest);
+            accuArrLen += arr2.array.length;
+
+
+            boolean[] result = getValidityBuffer(dest, 0, accuArrLen);
+            arr1.verifyData(result);
+            arr2.verifyData(result);
+        }
+    }
+
     @Test
     public void testConcatValidity() {
         Random random = new Random(7788);
