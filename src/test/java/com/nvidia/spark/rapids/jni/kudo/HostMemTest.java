54f34dd4c568fc035c8cd0df30852f31cec8cfca
liurenjie1024 <liurenjie2008@gmail.com>
2025-09-17 17:41:17 +0800

    ints

--- src/test/java/com/nvidia/spark/rapids/jni/kudo/HostMemTest.java
+++ src/test/java/com/nvidia/spark/rapids/jni/kudo/HostMemTest.java
@@ -3,16 +3,21 @@ package com.nvidia.spark.rapids.jni.kudo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import ai.rapids.cudf.HostMemoryBuffer;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 public class HostMemTest {
   @Test
   public void getIntTest() {
-    try(HostMemoryBuffer mem = HostMemoryBuffer.allocate(8)) {
-      mem.setInt(0, 42);
-      mem.setInt(4, 43);
+    try(HostMemoryBuffer mem = HostMemoryBuffer.allocate(1000)) {
+      for (int i = 0; i < 1000 / 4; i++) {
+        mem.setInt(i * 4, i * 10);
+      }
 
-      assertEquals(10, mem.getInt(1));
+      int[] ints = new int[100];
+      mem.getInts(ints, 0, 37, 100);
+
+      System.out.println("ints:" + Arrays.toString(ints));
     }
   }
 }
